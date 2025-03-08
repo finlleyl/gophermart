5061a03ede58b3165a059125d4e9db0a75829fcc
finlleyl <ilya_chibugaev@mail.ru>
2025-03-08 23:52:39 +0300

    add chi

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
 	"gophermart/pkg/config"
 	"log"
 	"net/http"
 )
 
 func main() {
+	r := chi.NewRouter()
 	cfg := config.LoadConfig()
 
-	if err := http.ListenAndServe(cfg.RunAddress, nil); err != nil {
+	if err := http.ListenAndServe(cfg.RunAddress, r); err != nil {
 		log.Fatal(err)
 	}
 }
