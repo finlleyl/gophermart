731b8a21689a8d25ffbc71e3c507ddba49f8b969
finlleyl <ilya_chibugaev@mail.ru>
2025-03-09 02:13:16 +0300

    add database

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/go-chi/chi/v5"
 	"gophermart/pkg/config"
+	"gophermart/pkg/database"
 	"log"
 	"net/http"
 )
@@ -10,6 +11,11 @@ import (
 func main() {
 	r := chi.NewRouter()
 	cfg := config.LoadConfig()
+	db, err := database.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+	defer db.Close()
 
 	if err := http.ListenAndServe(cfg.RunAddress, r); err != nil {
 		log.Fatal(err)
---
+++ pkg/database/database.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"os"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
+func loadDBConfig() *DBConfig {
+	cfg := DBConfig{}
+
+	if host := os.Getenv("HOST"); host != "" {
+		cfg.Host = host
+	}
+	if user := os.Getenv("POSTGRES_USER"); user != "" {
+		cfg.User = user
+	}
+	if password := os.Getenv("POSTGRES_PASSWORD"); password != "" {
+		cfg.Password = password
+	}
+	if dbname := os.Getenv("POSTGRES_DB"); dbname != "" {
+		cfg.DBName = dbname
+	}
+
+	cfg.Port = 5432
+	cfg.SSLMode = "disable"
+
+	return &cfg
+}
+
+func ConnectDB() (*sqlx.DB, error) {
+	cfg := loadDBConfig()
+
+	dsn := fmt.Sprint(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode)
+	db, err := sqlx.Connect("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
