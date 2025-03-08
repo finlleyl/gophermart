dad5a7afaab73311fe0457f72f203e86116fb72e
finlleyl <ilya_chibugaev@mail.ru>
2025-03-09 01:34:53 +0300

    add docker

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -6,17 +6,23 @@ import (
 )
 
 type Config struct {
-	RunAddress string
+	RunAddress  string
+	DatabaseURI string
 }
 
 func LoadConfig() *Config {
 	cfg := &Config{}
 
 	flag.StringVar(&cfg.RunAddress, "a", ":8080", "run address")
+	flag.StringVar(&cfg.DatabaseURI, "d", "", "database uri")
 	flag.Parse()
 
 	if addr := os.Getenv("RUN_ADDRESS"); addr != "" {
 		cfg.RunAddress = addr
+	}
+
+	if db := os.Getenv("DATABASE_URI"); db != "" {
+		cfg.DatabaseURI = db
 	}
 
 	return cfg
